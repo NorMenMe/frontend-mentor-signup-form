4341297474c8a548331524dd60a5fb7455361e5f
NorMenMe <info@normanmetz.com>
2021-12-08 11:26:40 +0100

    stashed fields

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,14 @@
-// access dynmically to id of each input
-// check if there is a value, === ''
-// return an error message to ad to the innerhtml of the span,next sibling element of input
+// to do :
+
+// parent of input :focus outline
+// media query
+
+// issue: onClick
 
 // next :
-// style it , color icon and border of container when listening to event
-// multiply your block to all other sections
+// increase placeholder font
+// after submit border of input.parentElement on focus
+// refactor the logic of the error message
 
 const fields = ["first-name", "last-name", "email", "password"];
 
@@ -17,7 +21,20 @@ form.addEventListener("submit", (e) => {
     const input = document.getElementById(`${field}`);
 
     if (!input.value) {
+      input.parentElement.style.cssText = "border: 3px solid #ff0000;";
+      input.nextElementSibling.style.display = "block";
       input.parentElement.nextElementSibling.innerHTML = `${input.placeholder} cannot be empty`;
+    } else if (field === "email") {
+      const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!re.test(input.value)) {
+        input.parentElement.nextElementSibling.innerHTML =
+          "Looks like this is not an email";
+        input.parentElement.style.cssText = "border: 3px solid #ff0000;";
+        input.nextElementSibling.style.display = "block";
+        input.style.cssText = "color : red; font-weight : 600";
+      }
     }
   });
 });
+
+const submit = document.querySelector(".btn-submit");
